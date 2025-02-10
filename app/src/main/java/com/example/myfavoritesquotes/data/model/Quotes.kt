6131bd6cd063c3fa4d83bf6cc60c344a725aa6c5
nathan79-c <19kt210@gmail.com>
2025-02-10 22:52:39 +0200

    create DAO for a model mapper

--- app/src/main/java/com/example/myfavoritesquotes/data/model/Quotes.kt
+++ app/src/main/java/com/example/myfavoritesquotes/data/model/Quotes.kt
@@ -1,7 +1,28 @@
 package com.example.myfavoritesquotes.data.model
 
+import com.example.myfavoritesquotes.data.local.dao.EntityQuotes
+
 data class Quotes(
     var quotes :String,
     var auteur : String,
 
 )
+
+object QuotesMapper {
+
+    // Convert EntityQuotes to Quotes
+    fun toModel(entityQuotes: EntityQuotes): Quotes {
+        return Quotes(
+            quotes = entityQuotes.quotes,
+            auteur = entityQuotes.auteur
+        )
+    }
+
+    // Convert Quotes to EntityQuotes
+    fun toEntity(quotes: Quotes): EntityQuotes {
+        return EntityQuotes(
+            quotes = quotes.quotes,
+            auteur = quotes.auteur
+        )
+    }
+}
