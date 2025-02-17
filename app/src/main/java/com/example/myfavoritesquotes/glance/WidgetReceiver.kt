4f9eb8651b01a433837af37ab0c8a2b7ffcc6881
nathan79-c <19kt210@gmail.com>
2025-02-17 17:39:26 +0200

    create a part of widget

---
+++ app/src/main/java/com/example/myfavoritesquotes/glance/WidgetReceiver.kt
@@ -0,0 +1,10 @@
+package com.example.myfavoritesquotes.glance
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class WidgetReceiver : GlanceAppWidgetReceiver(){
+
+    override val glanceAppWidget: GlanceAppWidget
+        get() = TODO("Not yet implemented")
+}
