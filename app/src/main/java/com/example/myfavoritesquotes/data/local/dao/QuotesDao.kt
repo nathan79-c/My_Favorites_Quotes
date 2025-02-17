f52ead52bc876d99c614c71b4a57d72d42d28ec7
nathan79-c <19kt210@gmail.com>
2025-02-17 20:02:49 +0200

    change a name of model class

--- app/src/main/java/com/example/myfavoritesquotes/data/local/dao/QuotesDao.kt
+++ app/src/main/java/com/example/myfavoritesquotes/data/local/dao/QuotesDao.kt
@@ -2,21 +2,21 @@ package com.example.myfavoritesquotes.data.local.dao
 
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.myfavoritesquotes.data.model.Quotes
+import com.example.myfavoritesquotes.data.model.QuotesModel
 
 interface QuotesDao {
 
     // Insert a new quote
     @Insert
-    suspend fun insertQuote(quote: Quotes)
+    suspend fun insertQuote(quote: QuotesModel)
 
     // Get all quotes
     @Query("SELECT * FROM quotes")
-    suspend fun getAllQuotes(): List<Quotes>
+    suspend fun getAllQuotes(): List<QuotesModel>
 
     // Get a quote by its ID
     @Query("SELECT * FROM quotes WHERE id = :id LIMIT 1")
-    suspend fun getQuoteById(id: Long): Quotes?
+    suspend fun getQuoteById(id: Long): QuotesModel?
 
     // Delete a quote by its ID
     @Query("DELETE FROM quotes WHERE id = :id")
