715bd0ba3e9fae754c6791bf610ffb6b858fb1a4
nathan79-c <19kt210@gmail.com>
2025-02-26 13:54:28 +0200

    add function for abstract class

--- app/src/main/java/com/example/myfavoritesquotes/data/local/QuoteDatabase.kt
+++ app/src/main/java/com/example/myfavoritesquotes/data/local/QuoteDatabase.kt
@@ -1,11 +1,24 @@
 package com.example.myfavoritesquotes.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myfavoritesquotes.data.local.dao.QuotesDao
 import com.example.myfavoritesquotes.data.local.entity.EntityQuotes
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 @Database(entities = [EntityQuotes::class], version = 1, exportSchema = false)
 abstract class QuoteDatabase: RoomDatabase(){
             abstract fun QuotesDao():QuotesDao
 }
+
+fun provideRoomDatabase(@ApplicationContext context: Context): QuoteDatabase {
+    return Room.databaseBuilder(
+        context.applicationContext,
+        QuoteDatabase::class.java,
+        "movie_database"
+    ).build()
+}
+
+fun provideMovieDao(movieDatabase: QuoteDatabase) = movieDatabase.QuotesDao()
