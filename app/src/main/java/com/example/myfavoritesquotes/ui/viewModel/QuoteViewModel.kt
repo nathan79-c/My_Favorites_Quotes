fd0316aa7d57fbf96a1b586167b9cbdaba3cffa9
nathan79-c <19kt210@gmail.com>
2025-03-02 15:59:11 +0200

    cration de state ui pour le viewmodel|

--- app/src/main/java/com/example/myfavoritesquotes/ui/viewModel/QuoteViewModel.kt
+++ app/src/main/java/com/example/myfavoritesquotes/ui/viewModel/QuoteViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.myfavoritesquotes.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.myfavoritesquotes.data.model.QuotesModel
 import com.example.myfavoritesquotes.data.repository.QuotesRepository
 
 class QuoteViewModel(private val quotesRepository:QuotesRepository):ViewModel() {
 
+}
+
+sealed class QuoteUiState{
+    object Loading:QuoteUiState()
+    data class  Error(val message:String):QuoteUiState()
+    data class Succes(
+        val Quotes:List<QuotesModel>,
+        val selectQuote:QuotesModel?= null
+    ):QuoteUiState()
 }
