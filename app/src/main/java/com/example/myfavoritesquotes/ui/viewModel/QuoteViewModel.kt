a9391be63699527775413e6a3e75fb2231767f7e
nathan79-c <19kt210@gmail.com>
2025-03-02 16:02:33 +0200

    creation de la variable uistate

--- app/src/main/java/com/example/myfavoritesquotes/ui/viewModel/QuoteViewModel.kt
+++ app/src/main/java/com/example/myfavoritesquotes/ui/viewModel/QuoteViewModel.kt
@@ -3,8 +3,13 @@ package com.example.myfavoritesquotes.ui.viewModel
 import androidx.lifecycle.ViewModel
 import com.example.myfavoritesquotes.data.model.QuotesModel
 import com.example.myfavoritesquotes.data.repository.QuotesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class QuoteViewModel(private val quotesRepository:QuotesRepository):ViewModel() {
+    private  val _uiState = MutableStateFlow<QuoteUiState>(QuoteUiState.Loading)
+    val uiState:StateFlow<QuoteUiState> = _uiState.asStateFlow()
 
 }
 
