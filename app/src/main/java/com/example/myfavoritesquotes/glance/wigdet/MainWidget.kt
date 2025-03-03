246498431e0748cb532b91ae8f431f7ba32dccea
nathan79-c <19kt210@gmail.com>
2025-03-03 05:51:54 +0200

    cration of box of cotent widget

--- app/src/main/java/com/example/myfavoritesquotes/glance/wigdet/MainWidget.kt
+++ app/src/main/java/com/example/myfavoritesquotes/glance/wigdet/MainWidget.kt
@@ -1,13 +1,20 @@
 package com.example.myfavoritesquotes.glance.wigdet
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+
 import androidx.glance.text.Text
 import com.example.myfavoritesquotes.data.model.QuotesModel
 
@@ -23,7 +30,11 @@ class MainWidget:GlanceAppWidget() {
 
 @Composable
 fun widgetScreen(modifier: Modifier,quoteList:List<QuotesModel>){
-    Box(modifier = modifier){
+    Box(modifier = modifier
+        .fillMaxSize()
+        .padding(20.dp)
+
+    ){
         for ( currentQuote in quoteList){
             Text(currentQuote.quotes)
             Text(currentQuote.auteur)
