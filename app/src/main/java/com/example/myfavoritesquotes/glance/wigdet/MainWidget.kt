3a3cf19cd82058dda52e37527a733108bfea2395
nathan79-c <19kt210@gmail.com>
2025-03-03 05:39:28 +0200

    creation of quotes widget

--- app/src/main/java/com/example/myfavoritesquotes/glance/wigdet/MainWidget.kt
+++ app/src/main/java/com/example/myfavoritesquotes/glance/wigdet/MainWidget.kt
@@ -1,10 +1,15 @@
 package com.example.myfavoritesquotes.glance.wigdet
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.glance.GlanceId
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.text.Text
+import com.example.myfavoritesquotes.data.model.QuotesModel
 
 class MainWidget:GlanceAppWidget() {
 
@@ -15,3 +20,15 @@ class MainWidget:GlanceAppWidget() {
         }
     }
 }
+
+@Composable
+fun widgetScreen(modifier: Modifier,quoteList:List<QuotesModel>){
+    Box(modifier = modifier){
+        for ( currentQuote in quoteList){
+            Text(currentQuote.quotes)
+            Text(currentQuote.auteur)
+        }
+    }
+}
+
+
